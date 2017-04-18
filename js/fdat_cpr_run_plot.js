83e78aece50eec2a1d7fa94ace2e87cf518a357f
Peter DeWitt (latitude) <peter.dewitt@ucdenver.edu>
2017-04-17 18:02:26 -0600

    added all files so the repo, upon clone, will just work

---
+++ js/fdat_cpr_run_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#fdat_cpr_run_plot').scianimator({
+	    'images': ['fdat_cpr_run_dir/fdat_cpr_run_plot1.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot2.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot3.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot4.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot5.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot6.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot7.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot8.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot9.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot10.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot11.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot12.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot13.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot14.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot15.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot16.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot17.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot18.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot19.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot20.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot21.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot22.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot23.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot24.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot25.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot26.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot27.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot28.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot29.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot30.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot31.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot32.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot33.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot34.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot35.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot36.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot37.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot38.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot39.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot40.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot41.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot42.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot43.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot44.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot45.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot46.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot47.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot48.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot49.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot50.png', 'fdat_cpr_run_dir/fdat_cpr_run_plot51.png'],
+	    'width': 600,
+	    'delay': 100,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
